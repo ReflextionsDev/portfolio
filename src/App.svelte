a9121c533b8350531aab510e1227d282b6517d66
ReflextionsDev <reflextionsdev@gmail.com>
2022-04-08 16:55:49 -0400

    Add game card flexbox

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,61 @@
 <script>
 	export let name;
+	import Card from "./components/Card.svelte";
+
+	const games = [
+		{
+			title: "Game 1",
+			card_img: "/assets/games/cards/rrmenu.png",
+		},
+		{
+			title: "Game 2",
+			card_img: "/assets/games/cards/voter1.png",
+		},
+		{
+			title: "Game 3",
+			card_img: "/assets/games/cards/musklander1.png",
+		},
+	];
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<p>
+		Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn
+		how to build Svelte apps.
+	</p>
+
+	<div class="games">
+
+			{#each games as game}
+			<Card 
+				title={game.title}
+				src = {game.card_img}
+			/>
+		{/each}
+		
+
+	</div>
+	
+	<!-- <Card title={23}/>
+	<Card/>
+	<Card/> -->
 </main>
 
 <style>
+
 	main {
 		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
 	}
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
+	.games {
+		display: flex;
+
+		/* flex-direction: column; */
+		flex-wrap: wrap;
+
+		justify-content: center;
+		
 	}
 
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
 </style>
---
+++ src/components/Card.svelte
@@ -0,0 +1,22 @@
+<script>
+    export let title
+    export let src = '/assets/games/cards/rrmenu.png'
+</script>
+
+
+    <div class="game">
+        <!-- <h1>{title}</h1> -->
+        <img {src} alt="" class="card">
+    </div>
+
+
+<style>
+    .game {
+        display: flex;
+        /* min-width: 0; */
+    }
+
+    .game > img {
+        width: 450px;
+    }
+</style>
