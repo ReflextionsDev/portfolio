13487088bdba289b01a568afc176729bc970b634
ReflextionsDev <reflextionsdev@gmail.com>
2022-07-12 18:14:39 -0400

    clean and comment Splash

--- src/pages/Splash.svelte
+++ src/pages/Splash.svelte
@@ -1,67 +1,59 @@
-<!-- COMMENT ME -->
+<!-- The Splash page is a combination of header content and a navbar
+    At the top, landing info is displayed, below is a dynamic navigation bar
+    Header is a togglable class to adjust the component when in header mode -->
 <script>
+    // Components
     import Info from "./../components/Info.svelte";
     import Nav from "./../components/Nav.svelte";
 
-    let header = false;
-
-    import { theme } from '../stores.js'
- 
-    let maskColor = theme.bgMasks.blue
+    // Theme
+    import { theme } from "../stores.js";
+    let maskColor = theme.bgMasks.blue;
 
     // Responsive variables to set splash top to the bottom of content: so sticky position can be used
+    // contentHeight is binded to splash__content, and splashTop is subscribed to content height
     let contentHeight;
-    let splashTop = "";
+    let splashTop = "0px";
     $: splashTop = -contentHeight + "px";
 
-    let elem;
+    // Header toggles header css class rules, elemNav is binded
+    let header = false;
+    let elemNav;
 
-    // Convert to svelte... onMount and stuff
-
-    document.addEventListener("DOMContentLoaded", function () {
-        elem = document.querySelector(".splash__nav");
-        console.log(elem);
-    });
-
-    function updateNav() {
-        const navTop = Math.floor(elem.getBoundingClientRect().top);
+    // Check header mode on scroll, checks if the navbar has reached the top of the page
+    function updateHeaderMode() {
+        const navTop = Math.floor(elemNav.getBoundingClientRect().top);
         navTop <= 0 ? (header = true) : (header = false);
     }
 </script>
 
 <svelte:window
     on:scroll={(e) => {
-        updateNav();
+        updateHeaderMode();
     }}
 />
 
 <div class="splash" class:header style="--splashTop: {splashTop};">
     <div class="splash__blur" class:header style="--maskColor: {maskColor};">
-        <!-- Wrapper for hiding splash content in header mode -->
-        <div class="splash__content" bind:offsetHeight={contentHeight} >
-            <br />
-            <br />
-            <br />
+        <div class="splash__content" bind:offsetHeight={contentHeight}>
             <Info />
-            <br />
-            <br />
-            <br />
         </div>
 
-        <div class="splash__nav">
+        <div class="splash__nav" bind:this={elemNav}>
             <Nav />
         </div>
     </div>
 </div>
 
 <style>
-    /*  */
+    /* Sticky the component, setting top to the bottom of the content */
     .splash {
         position: sticky;
         top: var(--splashTop);
         z-index: 100;
     }
 
+    /* Renders a background image below the whole component */
     .splash::before {
         z-index: -2;
         top: 0px;
@@ -79,26 +71,26 @@
         background-repeat: no-repeat;
     }
 
-    /* .splash::after {
-        background-color: rgb(27 46 132 / 54%);
-    } */
-
+    /* Reduce bg image while in header mode */
     .splash.header::before {
         opacity: 50%;
     }
 
+    /* Add background mask color */
     .splash__blur {
         width: auto;
         height: inherit;
-        /* background-color: rgb(27 46 132 / 54%); */
         background-color: var(--maskColor);
     }
 
+    /* Blur content behind the header in header mode */
     .splash__blur.header {
         backdrop-filter: blur(3px);
     }
 
+    /* Adjust padding for vw, extra bottom padding for navbar */
     .splash__content {
-        padding-bottom: 6vw;
+        padding-top: 4vw;
+        padding-bottom: 8vw;
     }
 </style>
