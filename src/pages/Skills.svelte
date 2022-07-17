4d7f756ad87c061c9880206116221147af246f57
ReflextionsDev <reflextionsdev@gmail.com>
2022-07-17 02:18:49 -0400

    add skill icon hover behavior

--- src/pages/Skills.svelte
+++ src/pages/Skills.svelte
@@ -18,101 +18,237 @@
         Jquery,
     } from "./../components/icons/icons.js";
 
+    const iconSize = 64;
+    const colorHover = "#FFFFFF";
+
     let icons = [
         {
-            label: "javascript",
+            label: "Javascript",
             type: "frontend",
             component: Javascript,
             colorDefault: "#F7DF1E",
+            link: "",
+        },
+        {
+            label: "HTML5",
+            type: "frontend",
+            component: Html5,
+            colorDefault: "#E34F26",
+        },
+        {
+            label: "CSS3",
+            type: "frontend",
+            component: CssThree,
+            colorDefault: "#1572B6",
+        },
+        {
+            label: "React",
+            type: "frontend",
+            component: React,
+            colorDefault: "#61DAFB",
+        },
+        {
+            label: "Redux",
+            type: "frontend",
+            component: Redux,
+            colorDefault: "#764ABC",
+        },
+        {
+            label: "Svelte",
+            type: "frontend",
+            component: Svelte,
+            colorDefault: "#FF3E00",
+        },
+        {
+            label: "Node.JS",
+            type: "backend",
+            component: NodeDotJs,
+            colorDefault: "#339933",
+        },
+        {
+            label: "Express",
+            type: "backend",
+            component: Express,
+            colorDefault: "#000000",
+        },
+        {
+            label: "MongoDB",
+            type: "backend",
+            component: Mongodb,
+            colorDefault: "#47A248",
+        },
+        {
+            label: "Amazon AWS",
+            type: "backend",
+            component: Amazonaws,
+            colorDefault: "#232F3E",
+        },
+        {
+            label: "SQLite",
+            type: "backend",
+            component: Sqlite,
+            colorDefault: "#003B57",
+        },
+        {
+            label: "Git",
+            type: "other",
+            component: Git,
+            colorDefault: "#F05032",
+        },
+        {
+            label: "JQuery",
+            type: "other",
+            component: Jquery,
+            colorDefault: "#0769AD",
+        },
+        {
+            label: "Figma",
+            type: "other",
+            component: Figma,
+            colorDefault: "#F24E1E",
+        },
+        {
+            label: "Notion",
+            type: "other",
+            component: Notion,
+            colorDefault: "#000000",
+        },
+        {
+            label: "Construct 3",
+            type: "other",
+            component: ConstructThree,
+            colorDefault: "#96aab9",
         },
     ];
 
     icons.forEach((icon) => {
+        // icon.hoverColor = icon.colorDefault
+        // icon.colorDefault = '#FFFFFF'
         icon.color = icon.colorDefault;
+        icon.hoverColor = colorHover;
     });
-
-    const iconSize = 64;
-    const colorHover = "#FFFFFF";
 
     function iconMouseOver(label) {
         let index = icons.findIndex((icon) => icon.label === label);
-        icons[index].color = colorHover;
+        icons[index].color = icons[index].hoverColor;
     }
 
     function iconMouseOut(label) {
         let index = icons.findIndex((icon) => icon.label === label);
         icons[index].color = icons[index].colorDefault;
     }
-
-
-
 </script>
 
-<!-- !Add title prop to section padding! -->
-
 <div class="skills content">
-    {#each icons as icon}
-        {#if icon.type === "frontend"}
-            <div
-                on:mouseover={(e) => iconMouseOver(icon.label)}
-                on:focus={(e) => iconMouseOver(icon.label)}
-                on:mouseout={(e) => iconMouseOut(icon.label)}
-                on:blur={(e) => iconMouseOut(icon.label)}
-                class="hvr-grow"
-            >
-                <svelte:component
-                    this={icon.component}
-                    color={icon.color}
-                    size={iconSize}
-                />
-            </div>
-        {/if}
-    {/each}
-
     <h2 class="dash">Skills</h2>
 
     <div class="content__section">
         <h3>Front-End</h3>
         <div>
-            <!-- <Javascript color={colour} size={iconSize} /> -->
-            <Html5 color={"#E34F26"} size={iconSize} />
-            <CssThree color={"#1572B6"} size={iconSize} />
-            <React color={"#61DAFB"} size={iconSize} />
-            <Redux color={"#764ABC"} size={iconSize} />
-            <Svelte color={"#FF3E00"} size={iconSize} />
+            {#each icons as icon}
+                {#if icon.type === "frontend"}
+                    <div
+                        on:mouseover={(e) => iconMouseOver(icon.label)}
+                        on:focus={(e) => iconMouseOver(icon.label)}
+                        on:mouseout={(e) => iconMouseOut(icon.label)}
+                        on:blur={(e) => iconMouseOut(icon.label)}
+                        class="hvr-grow tooltip"
+                    >
+                        <span class="tooltiptext">{icon.label}</span>
+                        <svelte:component
+                            this={icon.component}
+                            color={icon.color}
+                            size={iconSize}
+                        />
+                    </div>
+                {/if}
+            {/each}
         </div>
     </div>
 
     <div class="content__section">
         <h3>Back-End</h3>
         <div>
-            <NodeDotJs color={"#339933"} size={iconSize} />
-            <Express color={"#000000"} size={iconSize} />
-            <Mongodb color={"#47A248"} size={iconSize} />
-            <Amazonaws color={"#232F3E"} size={iconSize} />
-            <Sqlite color={"#003B57"} size={iconSize} />
+            {#each icons as icon}
+                {#if icon.type === "backend"}
+                    <div
+                        on:mouseover={(e) => iconMouseOver(icon.label)}
+                        on:focus={(e) => iconMouseOver(icon.label)}
+                        on:mouseout={(e) => iconMouseOut(icon.label)}
+                        on:blur={(e) => iconMouseOut(icon.label)}
+                        class="hvr-grow tooltip"
+                    >
+                        <span class="tooltiptext">{icon.label}</span>
+                        <svelte:component
+                            this={icon.component}
+                            color={icon.color}
+                            size={iconSize}
+                        />
+                    </div>
+                {/if}
+            {/each}
         </div>
     </div>
 
     <div class="content__section">
         <h3>Other</h3>
         <div>
-            <Git color={"#F05032"} size={iconSize} />
-            <Jquery color={"#0769AD"} size={iconSize} />
-            <Figma color={"#F24E1E"} size={iconSize} />
-            <Notion color={"#000000"} size={iconSize} />
-            <ConstructThree color={"#96aab9"} size={iconSize} />
+            {#each icons as icon}
+                {#if icon.type === "other"}
+                    <div
+                        on:mouseover={(e) => iconMouseOver(icon.label)}
+                        on:focus={(e) => iconMouseOver(icon.label)}
+                        on:mouseout={(e) => iconMouseOut(icon.label)}
+                        on:blur={(e) => iconMouseOut(icon.label)}
+                        class="hvr-grow tooltip"
+                    >
+                        <span class="tooltiptext">{icon.label}</span>
+                        <svelte:component
+                            this={icon.component}
+                            color={icon.color}
+                            size={iconSize}
+                        />
+                    </div>
+                {/if}
+            {/each}
         </div>
     </div>
 </div>
 
 <style>
+    .tooltip {
+        position: relative;
+        display: inline-block;
+    }
+    .tooltip .tooltiptext {
+        opacity: 0;
+        width: 120px;
+        background-color: rgba(42, 14, 114, 0.746);
+        color: #fff;
+        text-align: center;
+        border-radius: 6px;
+        padding: 5px 0;
+        position: absolute;
+        z-index: 1;
+        bottom: -55%;
+        left: 50%;
+        margin-left: -60px;
+        transition-property: opacity;
+        transition-delay: 100ms;
+        transition-duration: 200ms;
+        font-size: 0.9rem;
+    }
+
+    .tooltip:hover .tooltiptext {
+        opacity: 100;
+    }
+
+    /* Hover CSS by @IanLunn https://github.com/IanLunn/Hover */
     .hvr-grow {
         display: inline-block;
         vertical-align: middle;
         -webkit-transform: perspective(1px) translateZ(0);
         transform: perspective(1px) translateZ(0);
-        box-shadow: 0 0 1px rgba(0, 0, 0, 0);
         -webkit-transition-duration: 0.3s;
         transition-duration: 0.3s;
         -webkit-transition-property: transform;
